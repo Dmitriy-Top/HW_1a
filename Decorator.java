a5cc3cc536f67c22f2f99dfdabaed0205a6a451b
Dmitriy-Top <top.d@bk.ru>
2016-10-26 16:17:01 +0400

    OOP style #1 home work

--- Decorator.java
+++ Decorator.java
@@ -1,11 +1,11 @@
 package calculator;
 //класс для обработки результатов вычислений, и придания им более удобного вида
-public class Decorator {
-    public static void resultDecorate(String result){ //убираем числа посел точки, там где они не нужны (5.0 => 5)
+class Decorator {
+    static void resultDecorate(String result){ //убираем числа посел точки, там где они не нужны (5.0 => 5)
         result = result.replace(".0","");
         System.out.println(result);
     }
-    public static double doubleDecorate(Double dbl){
+    static double doubleDecorate(Double dbl){
         return Math.rint(100.0 * dbl) / 100.0;
     } //убираем погрешность вычислений и округляем до сотых
 }
